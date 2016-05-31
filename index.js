86f09caab81b71b02c1c4ff5276374e8f66c8d7c
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-05-31 06:37:46 -0400

    add package.json and test connection

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
 
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://heroku_ts5mz99j:vfga87p833gpnu38q5to3p080h@ds019123.mlab.com:19123/heroku_ts5mz99j';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
